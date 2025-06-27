c48900c110589a90340f591e0fbdd9723b546507
code-with-zain-hunzai <dev.zainulabbdin@gmail.com>
2025-06-27 22:37:18 +0500

    fixed

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,23 @@
-// import app from "./app";
-// import { connectDB } from './config/db';
+import app from "./app";
+import { connectDB } from './config/db';
 
-// const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
 
-// connectDB();
+connectDB();
 
-// if (process.env.NODE_ENV !== "production") {
-//   app.listen(PORT, () => {
-//     console.log(`Server running on port ${PORT}`);
-//   });
-// }
+if (process.env.NODE_ENV !== "production") {
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+}
 
 
 // src/index.ts
-import { connectDB } from "./config/db";
-import app from "./app";
-import serverlessExpress from "@vendia/serverless-express";
+// import { connectDB } from "./config/db";
+// import app from "./app";
+// import serverlessExpress from "@vendia/serverless-express";
 
-// Connect to database once
-connectDB();
-
-// Export the handler for Vercel
-export default serverlessExpress({ app });
+// // Connect to database once
+// connectDB();
+// // Export the handler for Vercel
+// export default serverlessExpress({ app });
