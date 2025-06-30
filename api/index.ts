ef61dd05990d25e96c2a8f716f9b68f324e7b8b4
code-with-zain-hunzai <dev.zainulabbdin@gmail.com>
2025-06-30 11:25:47 +0500

    add and fix build error

--- api/index.ts
+++ api/index.ts
@@ -1,2 +1,10 @@
-import handler from '../src/index';
-export default handler; 
+// /api/index.ts
+import serverlessExpress from "@vendia/serverless-express";
+import app from "../src/app";
+import { connectDB } from "../src/config/db"
+
+(async () => {
+    await connectDB();
+  })();
+
+export default serverlessExpress({ app });
--- src/app.ts
+++ src/app.ts
@@ -25,8 +25,8 @@ app.use(
   })
 );
 
-// app.use(express.json({ limit: '10mb' }));
-// app.use(express.urlencoded({ limit: '10mb', extended: true }));
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ limit: '10mb', extended: true }));
 app.use(cookieParser());
 
 app.get("/", (req, res) => {
